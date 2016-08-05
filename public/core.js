ce401dea1d5dc6dcffe47f4955a8746618aa5ecd
Joshua G. Yolles <josh@whaicehockey.com>
2016-08-05 05:28:06 -0700

    first commitment

---
+++ public/core.js
@@ -0,0 +1,76 @@
+var scotchTodo = angular.module('scotchTodo', []);
+
+scotchTodo.controller('mainController', function($scope,$rootScope,$http) {
+  $scope.formData = {};
+  $scope.todos = [];
+  $rootScope.item = {};
+
+  $http.get('/api/todos').success(function(data) {
+    $scope.todos = data;
+    console.log(data);
+  }).error(function(data) {
+    console.log('Error: ' + data);
+  });
+
+  $scope.createTodo = function() {
+    if($scope.formData.text) {
+      $http.post('api/todos',$scope.formData).success(function(data) {
+        $scope.formData = {}; // clear form
+        $scope.todos = data;
+        console.log(data);
+      }).error(function(data) {
+        console.log("error: " + data);
+      })
+    } else {
+      alert("Cannot add an empty task!");
+    }
+  }
+
+  $scope.deleteTodo = function(id) {
+    var temp = confirm("Are you sure you want to delete this item?");
+
+    if(temp === true) {
+      $http.delete('/api/todos/' + id).success(function(data) {
+        $scope.todos = data;
+        console.log(data);
+      }).error(function(data) {
+        console.log("Error: " + data);
+      });
+    }
+    else {
+    }
+  };
+
+$scope.enableEdit = function(todoItem) {
+  todoItem.isBeingEdited = !todoItem.isBeingEdited;
+  todoItem.needConfirmation = true;
+  $rootScope.item = todoItem;
+};
+
+$scope.saveChanges = function(todoItem) {
+  $http.put('api/todos/edit', todoItem)
+   .then(
+       function(response){
+         // success callback
+         todoItem.isBeingEdited = false;
+         todoItem.needConfirmation = false;
+         $rootScope.item = todoItem;
+       },
+       function(response){
+         // failure callback
+       }
+    );
+};
+
+$scope.cancelChanges = function(todoItem) {
+  todoItem.isBeingEdited = false;
+  todoItem.needConfirmation = false;
+  // not complete
+};
+
+//border: 2px solid #5BC0DE;
+//cursor: pointerqa ;
+
+
+
+});
---
+++ server.js
@@ -0,0 +1,93 @@
+// set up
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+// execute mongod from mongo folder with --dbpath into todoData
+// start with nodemon.js
+// wallah
+mongoose.connect('mongodb://localhost/data');
+
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
+app.use(methodOverride());
+
+// model
+var Todo = mongoose.model('Todo', {
+  text: String
+});
+
+app.get('/', function(req,res) {
+  res.sendfile('./public/index.html')
+})
+
+// listen
+app.listen(8080);
+console.log("App listening on port 8080");
+
+// routes
+//get all todos
+app.get('/api/todos', function(req,res) {
+  Todo.find(function(err, todos) {
+    if(err)
+      res.send(err);
+
+    res.json(todos);
+  });
+});
+
+// create todo and send back all todos afterwards
+app.post('/api/todos', function(req,res) {
+  Todo.create({
+    text: req.body.text,
+    done: false,
+    isBeingEdited: false,
+    needConfirmation: false
+  }, function(err, todo) {
+    if(err)
+      res.send(err);
+
+    Todo.find(function(err, todos) {
+      if(err)
+        res.send(err);
+      res.json(todos);
+    });
+  });
+});
+
+// delete
+app.delete('/api/todos/:todo_id', function(req,res) {
+  Todo.remove({
+  _id: req.params.todo_id
+}, function(err, todo) {
+  if(err)
+    res.send(err);
+
+  Todo.find(function(err,todos) {
+    if(err)
+      res.send(err)
+    res.json(todos);
+  });
+});
+});
+
+// edit
+app.put('/api/todos/edit',function(req,res) {
+  var query = {'_id':req.body._id};
+
+  Todo.findOneAndUpdate(query, {"text":req.body.text}, {upsert:true}, function(err, doc){
+    if (err) return res.send(500, { error: err });
+  });
+
+  Todo.find(function(err,todos) {
+    if(err)
+      res.send(err)
+    res.json(todos);
+  });
+});
